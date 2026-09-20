a5285ae2d63d109e759042a5094058a8ee8ce6b0
dhgmonkey <dhgmonkey94@gmail.com>
2026-09-20 01:45:26 -0500

    refactor(privacy): strip Google Ads tracking, decouple benchmarks to aienos.com, calibrate chapter navigation (#24)

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,6 @@ import pageMetadata from "./site-metadata.json";
 
 function siteMetadata(development: boolean): Plugin {
   const home = pageMetadata["/"];
-  const adsId = process.env.VITE_GOOGLE_ADS_ID || process.env.GOOGLE_ADS_ID || "";
-  const tagSnippet = adsId
-    ? `<script async src="https://www.googletagmanager.com/gtag/js?id=${adsId}"></script><script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${adsId}');</script>`
-    : `<script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}</script>`;
 
   const values = {
     __SITE_MODE__: "portfolio",
@@ -16,7 +12,6 @@ function siteMetadata(development: boolean): Plugin {
     __SITE_STYLE_SRC__: development ? "style-src 'self' 'unsafe-inline'" : "style-src 'self'",
     __SITE_TITLE__: home.title,
     __SITE_DESCRIPTION__: home.description,
-    __SITE_TAG_SNIPPET__: tagSnippet,
   };
 
   return {
